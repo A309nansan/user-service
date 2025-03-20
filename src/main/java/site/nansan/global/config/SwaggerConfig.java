14403123cc8390413510d90f1ec8911b8ff631fd
전성호 <kopasd99@gmail.com>
2025-03-20 13:24:39 +0900

    config : swagger에 JWT 추가

--- src/main/java/site/nansan/global/config/SwaggerConfig.java
+++ src/main/java/site/nansan/global/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package site.nansan.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +14,15 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI userServiceOpenAPI() {
         return new OpenAPI()
+                .components(
+                        new Components().addSecuritySchemes("Bearer",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("Bearer"))
                 .info(
                         new Info()
                                 .title("Nansan User Service API")
