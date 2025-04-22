cbd227799273e6803b5e09fdfd5e3d8ae576171c
octopus-kim <62287654+octopus-kim@users.noreply.github.com>
2025-04-22 09:47:19 +0900

    Update init-user-service.sh

--- init-user-service.sh
+++ init-user-service.sh
@@ -18,8 +18,6 @@ trap 'error $LINENO' ERR
 
 log "스크립트 실행 시작."
 
-cd user || { echo "디렉토리 변경 실패"; exit 1; }
-
 # docker network 생성
 if docker network ls --format '{{.Name}}' | grep -q '^nansan-network$'; then
   log "Docker network named 'nansan-network' is already existed."
@@ -38,29 +36,18 @@ TOKEN_RESPONSES=$(curl -s --request POST \
 CLIENT_TOKEN=$(echo "$TOKEN_RESPONSES" | jq -r '.auth.client_token')
 
 SECRET_RESPONSE=$(curl -s --header "X-Vault-Token: ${CLIENT_TOKEN}" \
-  --request GET https://vault.nansan.site/v1/kv/data/auth)
+  --request GET https://vault.nansan.site/v1/kv/data/authentication)
 
-MONGODB_HOST=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.mongodb.private.host')
-MONGODB_PORT=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.mongodb.private.port')
-MONGODB_USERNAME=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.mongodb.username')
-MONGODB_PASSWORD=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.mongodb.password')
-REDIS_HOST=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.redis.private.host')
-REDIS_PORT=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.redis.private.port')
-REDIS_PASSWORD=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.redis.password')
-MYSQL_HOST=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.mysql.private.host')
-MYSQL_PORT=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.mysql.private.port')
+JWT_SECRET=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.token.jwt')
+REDIS_PASSWORD=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.redis.username')
 MYSQL_USERNAME=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.mysql.username')
 MYSQL_PASSWORD=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.mysql.password')
-CONFIG_SERVER_URI=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.configserver.private.uri')
 CONFIG_SERVER_USERNAME=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.configserver.username')
 CONFIG_SERVER_PASSWORD=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.configserver.password')
-RABBITMQ_HOST=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.rabbitmq.server.host')
-RABBITMQ_PORT=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.rabbitmq.server.port')
 RABBITMQ_USERNAME=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.rabbitmq.username')
 RABBITMQ_PASSWORD=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.rabbitmq.password')
 MINIO_ACCESS_KEY=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.minio.username')
 MINIO_SECRET_KEY=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.minio.password')
-JWT_SECRET=$(echo "$SECRET_RESPONSE" | jq -r '.data.data.apigateway.jwt')
 
 # Build Gradle
 log "build gradle"
@@ -80,27 +67,25 @@ log "Execute user-service..."
 docker run -d \
   --name user-service \
   --restart unless-stopped \
-  -e MONGODB_HOST=${MONGODB_HOST} \
-  -e MONGODB_PORT=${MONGODB_PORT} \
-  -e MONGODB_USERNAME=${MONGODB_USERNAME} \
-  -e MONGODB_PASSWORD=${MONGODB_PASSWORD} \
-  -e REDIS_HOST=${REDIS_HOST} \
-  -e REDIS_PORT=${REDIS_PORT} \
-  -e REDIS_PASSWORD=$REDIS_PASSWORD} \
-  -e MYSQL_HOST=${MYSQL_HOST} \
-  -e MYSQL_PORT=${MYSQL_PORT} \
+  -e JWT_SECRET=${JWT_SECRET} \
+  -e REDIS_HOST=redis \
+  -e REDIS_PORT=6379 \
+  -e REDIS_PASSWORD=${REDIS_PASSWORD} \
+  -e MYSQL_HOST=mysql \
+  -e MYSQL_PORT=3306 \
   -e MYSQL_USERNAME=${MYSQL_USERNAME} \
   -e MYSQL_PASSWORD=${MYSQL_PASSWORD} \
-  -e CONFIG_SERVER_URI=${CONFIG_SERVER_URI} \
+  -e CONFIG_SERVER_URI=http://config-server:8888 \
   -e CONFIG_SERVER_USERNAME=${CONFIG_SERVER_USERNAME} \
   -e CONFIG_SERVER_PASSWORD=${CONFIG_SERVER_PASSWORD} \
-  -e RABBITMQ_HOST=${RABBITMQ_HOST} \
-  -e RABBITMQ_PORT=${RABBITMQ_PORT} \
+  -e RABBITMQ_HOST=rabbitmq \
+  -e RABBITMQ_PORT=5672 \
   -e RABBITMQ_USERNAME=${RABBITMQ_USERNAME} \
   -e RABBITMQ_PASSWORD=${RABBITMQ_PASSWORD} \
+  -e DEFAULT_EUREKA_URL=http://eureka:8761/eureka \
+  -e MINIO_API_URL=https://s3.nansan.site \
   -e MINIO_ACCESS_KEY=${MINIO_ACCESS_KEY} \
   -e MINIO_SECRET_KEY=${MINIO_SECRET_KEY} \
-  -e JWT_SECRET=${JWT_SECRET} \
   --network nansan-network \
   user-service:latest
 
